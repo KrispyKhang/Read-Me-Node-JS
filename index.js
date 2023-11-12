f3c315341ea7a73913d8577ee65164ccf0a5737b
Khang Tran <creamybanana@Khang.local>
2023-11-12 13:57:11 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require("path");
+const generateMarkdown = require("./generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: "projectTitle",
+        message: "What is your prjoect title?",
+    },
+    {
+        type: 'input',
+        name: "projectDescription",
+        message: "Name your project description",
+    },
+    {
+        type: 'input',
+        name: "projectInstalltionInstructions",
+        message: "What are the installations for this project?",
+    },
+    {
+        type: 'input',
+        name: "projectUsage",
+        message: "What is the use case of this project?",
+    },
+    {
+        type: 'input',
+        name: "projectContributions",
+        message: "Enter the contribution instructions?",
+    },
+    {
+        type: 'input',
+        name: "projecTests",
+        message: "Methods you use to test your project.",
+    },
+    {
+        type: 'list',
+        name: "projectLicense",
+        message: "Select the licenses used.",
+        choices: [
+            "MIT",
+            "MOZILLA",
+            "IBM",
+            "NONE",
+        ]
+    },
+    {
+        
+    }
+
+
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
